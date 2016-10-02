034971b1a00270348e20b85c2cbb98b4e4b9fa2f
i143code <ranjanah143code@yahoo.com>
2016-10-02 00:12:01 -0500

    adding the form

--- RevolvingLoanHack/db/schema.rb
+++ RevolvingLoanHack/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002041908) do
+ActiveRecord::Schema.define(version: 20161002043844) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "address"
+    t.string "income"
+    t.string "loanAmount"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
